f4d3cc4f43c44af7036e4cf667d01ceb06fd4145
Kaupahcox <66149375+Kaupahcox@users.noreply.github.com>
2021-06-18 10:20:01 +1100

    Refacto et suppression des effets de bords

--- Script_Y_Avg.py
+++ Script_Y_Avg.py
@@ -21,31 +21,56 @@ date_fin = dt.datetime(current_annee, 1, 1)
 
 
 
-def calcul_moy_par_jour(annees, sortedVIIRS_Prov_c, dates_c, initstr):
-    '''Calcul la valeur moyenne de la surface incendiée chaque jour de l'année'''
+# def calcul_moy_par_jour(annees, sortedVIIRS_Prov_c, dates_c, initstr):
+#     '''Calcul la valeur moyenne de la surface incendiée chaque jour de l'année'''
+#     dico = [] 
+#     som = 0
+#     dates = dates_c  # .pop est impossible sur un argument
+#     sortedVIIRS_Prov = sortedVIIRS_Prov_c
+#     while len(sortedVIIRS_Prov) > 0 and len(dates) > 0:
+#         if dt.datetime.strftime(sortedVIIRS_Prov[0][0], "%m%d") == initstr:
+#             som += sortedVIIRS_Prov.pop(0)[1]  # La liste est classée par mois, puis par jour, puis par année
+#         else:
+#             dico.append([initstr, som/len(annees)])
+#             init = dates.pop(0)
+#             initstr = dt.datetime.strftime(init, "%m%d")
+#             som = 0
+#     while len(dates) > 0:
+#         init = dates.pop(0)
+#         initstr = dt.datetime.strftime(init, "%m%d")
+#         dico.append([initstr, 0])
+#         som = 0
+#     while len(sortedVIIRS_Prov) > 0:
+#         if dt.datetime.strftime(sortedVIIRS_Prov[0][0], "%m%d") == initstr:
+#             som += sortedVIIRS_Prov.pop(0)[1]
+#     if som != 0:
+#         dico.append([initstr, som/len(annees)])
+#     return dico
+
+def calcul_moy_par_jour(annees, sortedVIIRS_Prov, dates):
     dico = [] 
     som = 0
-    dates = dates_c  # .pop est impossible sur un argument
-    sortedVIIRS_Prov = sortedVIIRS_Prov_c
-    while len(sortedVIIRS_Prov) > 0 and len(dates) > 0:
-        if dt.datetime.strftime(sortedVIIRS_Prov[0][0], "%m%d") == initstr:
-            som += sortedVIIRS_Prov.pop(0)[1]  # La liste est classée par mois, puis par jour, puis par année
-        else:
-            dico.append([initstr, som/len(annees)])
-            init = dates.pop(0)
-            initstr = dt.datetime.strftime(init, "%m%d")
-            som = 0
-    while len(dates) > 0:
-        init = dates.pop(0)
-        initstr = dt.datetime.strftime(init, "%m%d")
-        dico.append([initstr, 0])
-        som = 0
-    while len(sortedVIIRS_Prov) > 0:
-        if dt.datetime.strftime(sortedVIIRS_Prov[0][0], "%m%d") == initstr:
-            som += sortedVIIRS_Prov.pop(0)[1]
-    if som != 0:
-        dico.append([initstr, som/len(annees)])
-    return dico
+    for date in dates:
+        datestr = dt.datetime.strftime(date, "%m%d")
+        surface_jour = [l[1] for l in sortedVIIRS_Prov if dt.datetime.strftime(l[0], "%m%d") == datestr]
+        som = sum(surface_jour)
+        dico.append([datestr, som/len(annees)])
+
+def moyenne_cumulee(current_annee, dico, summedList, Province):
+    for item in dico:
+        datestr = item[0]
+        surface = item[1]  # Sn
+        lastS = summedList[-1][1]  # Un-1
+        try:
+            datestr += str(current_annee)
+            date = dt.datetime.strptime(datestr, "%m%d%Y")
+            npdate = np.datetime64(date)
+            summedListSud.append((npdate, surface + lastS, Province))
+        except:
+            # Si l'année n'est pas bisextile, on saute le 29/02
+            print("L'année actuelle n'est pas une année bissextile")
+            pass
+    return summedList
 
 ###################################
 # On récupère les données dans la couche viirs pour chaque province
@@ -59,7 +84,6 @@ donneesVIIRS_Iles = [list(l) for l in arcpy.da.SearchCursor(IncendiesVIIRS, cham
     where_clause="Nom = 'Province des Iles' AND Debut >= '2013-01-01' AND Debut < '" + dt.datetime.strftime(date_fin, "%Y-%m-%d") + "'")]
 sortedVIIRS_Iles = sorted(donneesVIIRS_Iles, key= lambda x: int(dt.datetime.strftime(x[0], "%m%d%Y")))
 
-# avgs = np.array([], np.dtype([("Date", np.datetime64), ("Valeur", np.int32), ("Province", np.string)]))
 # Une liste des jours de l'année
 dates = []
 for jour in range(1, 367):
@@ -69,66 +93,42 @@ print("Longueur de dates : {}\nLongueur de sortedVIIRS_Sud : {}\n".format(len(da
 print("Longueur de dates : {}\nLongueur de sortedVIIRS_Nord : {}\n".format(len(dates), len(sortedVIIRS_Nord)))
 print("Longueur de dates : {}\nLongueur de sortedVIIRS_Iles : {}\n".format(len(dates), len(sortedVIIRS_Iles)))
 
-init = dates.pop(0)
-initstr = dt.datetime.strftime(init, "%m%d")
 
-dico_Sud = calcul_moy_par_jour(annees, sortedVIIRS_Sud, dates, initstr)
+dico_Sud = calcul_moy_par_jour(annees, sortedVIIRS_Sud, dates)
 
 dates = []
 for jour in range(1, 367):
     dates.append(dt.datetime.strptime("{:03}".format(jour) + "/" + str(2020), "%j/%Y")) ## Année bissextile au hasard
-init = dates.pop(0)
-initstr = dt.datetime.strftime(init, "%m%d")
+
 print("Longueur de dates : {}".format(len(dates)))
 
-dico_Nord = calcul_moy_par_jour(annees, sortedVIIRS_Nord, dates, initstr)
+dico_Nord = calcul_moy_par_jour(annees, sortedVIIRS_Nord, dates)
 
 dates = []
 for jour in range(1, 367):
     dates.append(dt.datetime.strptime("{:03}".format(jour) + "/" + str(2020), "%j/%Y")) ## Année bissextile au hasard
-init = dates.pop(0)
-initstr = dt.datetime.strftime(init, "%m%d")
+
 print("Longueur de dates : {}".format(len(dates)))
 
-dico_Iles = calcul_moy_par_jour(annees, sortedVIIRS_Iles, dates, initstr)
+dico_Iles = calcul_moy_par_jour(annees, sortedVIIRS_Iles, dates)
 
 # Un = Sn + Un-1, Sn étant la moyenne pour le jour n
 # U0 = S0
+
+
+        
 temp = dico_Sud.pop(0)
 summedListSud = [(np.datetime64(dt.datetime.strptime(temp[0] + str(current_annee), "%m%d%Y")), temp[1], "Province Sud")]
-for item in dico_Sud:
-    datestr = item[0]
-    surface = item[1]  # Sn
-    lastS = summedListSud[-1][1]  # Un-1
-    if datestr != "0229":  # Année bisextile
-        datestr+=str(current_annee)
-        date=dt.datetime.strptime(datestr, "%m%d%Y")
-        npdate = np.datetime64(date)
-        summedListSud.append((npdate, surface + lastS, "Province Sud"))
+summedListSud = moyenne_cumulee(current_annee, dico_Sud, summedListSud, "Province Sud")
 
 temp = dico_Nord.pop(0)
 summedListNord = [(np.datetime64(dt.datetime.strptime(temp[0] + str(current_annee), "%m%d%Y")), temp[1], "Province Nord")]
-for item in dico_Nord:
-    datestr = item[0]
-    surface = item[1]
-    lastS = summedListNord[-1][1]
-    if datestr != "0229":
-        datestr+=str(current_annee)
-        date=dt.datetime.strptime(datestr, "%m%d%Y")
-        npdate = np.datetime64(date)
-        summedListNord.append((npdate, surface + lastS, "Province Nord"))
+summedListSud = moyenne_cumulee(current_annee, dico_Nord, summedListNord, "Province Nord")
 
 temp = dico_Iles.pop(0)
 summedListIles = [(np.datetime64(dt.datetime.strptime(temp[0] + str(current_annee), "%m%d%Y")), temp[1], "Province des Iles")]
-for item in dico_Iles:
-    datestr = item[0]
-    surface = item[1]
-    lastS = summedListIles[-1][1]
-    if datestr != "0229":
-        datestr+=str(current_annee)
-        date=dt.datetime.strptime(datestr, "%m%d%Y")
-        npdate = np.datetime64(date)
-        summedListIles.append((npdate, surface + lastS, "Province des Iles"))
+summedListIles = moyenne_cumulee(current_annee, dico_Iles, summedListIles, "Province des Iles")
+
 
 # On concatène les trois listes
 npdt = np.dtype([('date', 'datetime64[us]'), ('S', np.float64), ('Province', 'a64')])
